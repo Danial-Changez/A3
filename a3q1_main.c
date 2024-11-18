f3c9302afce0002c93a11a14cc0caaedf61516f6
Danial-Changez <danial.changez@gmail.com>
2024-11-17 23:22:33 -0500

    Implemented error handling

--- a3q1_main.c
+++ a3q1_main.c
@@ -6,8 +6,10 @@
 #include "a3q1_header.h"
 
 // The main function drives the menu selection loop.
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Please use following input form: %s 'expression'\n", argv[0]);
         return 1;
     }
@@ -15,17 +17,26 @@ int main(int argc, char *argv[]) {
     char expr[100];
     int j = 0;
 
-    for (int i = 0; argv[1][i] != '\0'; i++) {
-        if (argv[1][i] != '\\') { 
+    for (int i = 0; argv[1][i] != '\0'; i++)
+    {
+        if (argv[1][i] != '\\')
+        {
             expr[j++] = argv[1][i];
         }
     }
     expr[j] = '\0';
 
+    if (!areParenthesesBalanced(expr) || !isValidExpression(expr))
+    {
+        printf("Error: Invalid input string %s. Please provide a valid expression.\n", expr);
+        return 1;
+    }
+
     Node *root = parseExpression(expr);
-    
+
     int choice;
-    do {
+    do
+    {
         printf("\nMenu:\n");
         printf("1. Preorder\n");
         printf("2. Inorder\n");
@@ -35,17 +46,29 @@ int main(int argc, char *argv[]) {
         printf("Enter choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1: preorder(root); printf("\n"); break;
-            case 2: inorder(root); printf("\n"); break;
-            case 3: postorder(root); printf("\n"); break;
-            case 4: 
-                varCount = 0; // resetting for each loop
-                promptVariables(root);
-                printf("Result: %.2f\n", calculate(root));
-                break;
-            case 5: break;
-            default: printf("Please enter a valid choice.\n");
+        switch (choice)
+        {
+        case 1:
+            preorder(root);
+            printf("\n");
+            break;
+        case 2:
+            inorder(root);
+            printf("\n");
+            break;
+        case 3:
+            postorder(root);
+            printf("\n");
+            break;
+        case 4:
+            varCount = 0; // resetting for each loop
+            promptVariables(root);
+            printf("Result: %.2f\n", calculate(root));
+            break;
+        case 5:
+            break;
+        default:
+            printf("Please enter a valid choice.\n");
         }
     } while (choice != 5);
 
